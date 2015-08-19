236425515f6d019befac6a524625cd2136e5bfa5
root <root@ip-172-31-7-150.ap-northeast-1.compute.internal>
2015-08-19 06:50:54 +0000

    docker!

---
+++ etc/docker/assets/entrypoint.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+
+SHIP_HOST=${SHIP_HOST:-localhost}
+SHIP_MATCH_PORT=${SHIP_MATCH_PORT:-8080}
+SHIP_GAME_PORT=${SHIP_GAME_PORT:-9090}
+SHIP_HTML5_CLIENT_PORT=${SHIP_HTML5_CLIENT_PORT:-80}
+
+sed s/{{SHIP_HOST}}/$SHIP_HOST/g -i /app/assets/config.toml
+sed s/{{SHIP_MATCH_PORT}}/$SHIP_MATCH_PORT/g -i /app/assets/config.toml
+sed s/{{SHIP_GAME_PORT}}/$SHIP_GAME_PORT/g -i /app/assets/config.toml
+
+
+# とりあえず適当に立ち上げる
+/go/bin/matching --config="/app/assets/config.toml" --port=${SHIP_MATCH_PORT} &
+/go/bin/game --config="/app/assets/config.toml" --port=${SHIP_GAME_PORT} &
+/go/bin/html5_client --config="/app/assets/config.toml" --port=${SHIP_HTML5_CLIENT_PORT} &
+
+
+
+bash
